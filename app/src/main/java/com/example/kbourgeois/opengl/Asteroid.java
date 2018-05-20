a08c9222ef9cd5933024b098a4ba3d5a2c63de50
Nicolas HIOT <nicolas.hiot@etu.univ-orleans.fr>
2018-05-20 22:30:05 +0200

    Asteroids field

--- app/src/main/java/com/example/kbourgeois/opengl/Asteroid.java
+++ app/src/main/java/com/example/kbourgeois/opengl/Asteroid.java
@@ -1,7 +1,6 @@
 package com.example.kbourgeois.opengl;
 
 import android.content.Context;
-
 import com.example.kbourgeois.opengl.openGLAdapter.Object3D;
 import com.example.kbourgeois.opengl.openGLAdapter.Vector3;
 
@@ -10,17 +9,19 @@ import java.util.Random;
 public class Asteroid extends Object3D {
 
     private Vector3 rotation;
+    private float speed;
 
     public Asteroid(Context context) {
         super();
         this.loadFromFile(context, "rock_c_01.obj");
         this.setShaders("vertexshader.vert", "fragmentshader.frag", "vPosition", "vNormal", "vTexCoord", R.drawable.stone);
+
+        this.getTransform().translate(getNewPosition());
+
         Random rand = new Random();
-        Vector3 v = new Vector3(rand.nextInt(3)-1,
-                rand.nextInt(2),
-                -100 - rand.nextInt(100));
-        this.getTransform().translate(v);
-        this.getTransform().setScale(new Vector3(0.4f, 0.4f, 0.4f));
+        float size = rand.nextFloat() / 2 + .2f;
+        speed = rand.nextFloat() * .04f + .01f;
+        this.getTransform().setScale(new Vector3(size, size, size));
         rotation = new Vector3(
                 rand.nextFloat(),
                 rand.nextFloat(),
@@ -29,14 +30,22 @@ public class Asteroid extends Object3D {
     }
 
     public void update(long delta_time){
-        this.getTransform().translateZ(0.05f*delta_time);
-        if(this.getTransform().getPosition().z > 20){
-            Random rand = new Random();
-            Vector3 v = new Vector3(rand.nextInt(3)-1,
-                    rand.nextInt(2),
-                    -100 - rand.nextInt(100));
-            this.getTransform().setPosition(v);
-        }
+        this.getTransform().translateZ(speed * delta_time);
         this.getTransform().rotate(rotation);
+
+        if(this.getTransform().getPosition().z > 20){
+            this.getTransform().setPosition(getNewPosition());
+        }
+    }
+
+    private Vector3 getNewPosition() {
+        Random rand = new Random();
+        Vector3 v = new Vector3(
+                rand.nextInt(20) - 10,
+                rand.nextInt(20) - 10,
+                -100 - rand.nextInt(200)
+        );
+
+        return v;
     }
 }
